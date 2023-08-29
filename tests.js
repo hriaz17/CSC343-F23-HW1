1793e7a4a186aec905deaa76ec950f4ef39001ef
hriaz17 <harisriaz1700@gmail.com>
2023-08-29 13:43:30 -0700

    Fix errors in imdb_data and test_data and update unit tests accordingly

--- tests.js
+++ tests.js
@@ -15,22 +15,22 @@ describe('Graph Class', () => {
     });
     
     it('test 1: should correctly read and populate nodes from a JSON file', () => {
-        assert.strictEqual(graphClass.graph.nodes.length, 3); // Check the number of nodes read
+        assert.strictEqual(graphClass.graph.nodes.length, 4); // Check the number of nodes read
     });
 
     it('test 2: should correctly add edges based on links in data', () => {
-        assert.strictEqual(graphClass.graph.edges.length, 2); // Check the number of edges added
+        assert.strictEqual(graphClass.graph.edges.length, 5); // Check the number of edges added
     });
 
     it('test 3: should correctly compute node degrees and add node degrees as attributes', () => {
-        assert.strictEqual(graphClass.graph.nodeDegrees['1'], 2); // Check degree for node 1
-        assert.strictEqual(graphClass.graph.nodeDegrees['2'], 1); // Check degree for node 2
-        assert.strictEqual(graphClass.graph.nodeDegrees['3'], 1); // Check degree for node 2
+        assert.strictEqual(graphClass.graph.nodeDegrees['tt0111161'], 2); // Check degree for node 'tt0111161'
+        assert.strictEqual(graphClass.graph.nodeDegrees['tt0068646'], 3); // Check degree for node 'tt0068646'
+        assert.strictEqual(graphClass.graph.nodeDegrees['tt0468569'], 2); // Check degree for node 'tt0468569'
     });
 
     it('test 4: should correctly calculate degree histogram', () => {
-        assert.strictEqual(graphClass.degreeHistogram[2], 1); // Check histogram entry for degree 3
-        assert.strictEqual(graphClass.degreeHistogram[1], 2); // Check histogram entry for degree 1
+        assert.strictEqual(graphClass.degreeHistogram[3], 2); // Check histogram entry for degree 3
+        assert.strictEqual(graphClass.degreeHistogram[2], 2); // Check histogram entry for degree 2
     });
     // Add more test cases if needed
 });
